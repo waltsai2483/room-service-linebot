df44736b4b23ee5a064826989ff30b0c36c59d91
walt-tsai <112289856+walt-tsai@users.noreply.github.com>
2024-02-18 20:43:34 +0800

    Modify: Test postgresql connection in Render

--- app.py
+++ app.py
@@ -11,7 +11,30 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@dpg-cn8v86i1hbls73ddaj9g-a.singapore-postgres.render.com:5432/room_service_line_db'.format(
+    os.getenv('DB_USERNAME', None), os.getenv('DB_PASSWORD', None))
+db.init_app(app)
+
+
+class Personnel(db.Model):
+    __tablename__ = 'personnels'
+    line_id = db.Column(db.String(32), primary_key=True)
+    account_name = db.Column(db.String(20), nullable=False)
+    job_code = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, id, name, job):
+        self.line_id = id
+        self.account_name = name
+        self.job_code = job
+
+
 line_bot_api = LineBotApi(os.getenv('LINE_ACCESS_TOKEN', None))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET', None))
 
@@ -37,6 +60,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    db.session.add(Personnel('dwa3erf', '蔡俊驊', 1))
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))

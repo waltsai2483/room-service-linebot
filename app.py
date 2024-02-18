f50f7d35b89084b6c6f04eeb42c804a5bde55a29
walt-tsai <112289856+walt-tsai@users.noreply.github.com>
2024-02-18 16:49:32 +0800

    Modify: requirements.txt run on Render

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 from flask import Flask
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 

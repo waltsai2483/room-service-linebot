f6014a9a64eb6c9897ae6004f1ea13ef41b62e47
walt-tsai <112289856+walt-tsai@users.noreply.github.com>
2024-02-18 17:07:51 +0800

    Modify: deploy linebot on Render using Flask

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import os
+
+from flask import Flask, request, abort
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -10,7 +12,35 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
+line_bot_api = LineBotApi(os.getenv('LINE_ACCESS_TOKEN', None))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET', None))
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run()

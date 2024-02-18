2d03ef24a812fb9e0588aa2fd6eeee713103bfa8
walt-tsai <112289856+walt-tsai@users.noreply.github.com>
2024-02-18 16:29:20 +0800

    Modify: app.py to run on Render

--- app.py
+++ app.py
@@ -2,11 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()

3cb8d3daa90cf3e96e242aa377815b4394eece33
walt-tsai <112289856+walt-tsai@users.noreply.github.com>
2024-02-19 10:03:27 +0800

    Feat: notify personnels with specific job code

--- app.py
+++ app.py
@@ -28,12 +28,20 @@ class Personnel(db.Model):
     __tablename__ = 'personnels'
     userid = db.Column(db.String(33), primary_key=True)
     username = db.Column(db.String(20), nullable=False)
-    job_code = db.Column(db.Integer, nullable=True)
+    job_code = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, id, name, job):
-        self.userid = id
+    def __init__(self, userid, name, job):
+        self.userid = userid
         self.username = name
         self.job_code = job
+
+
+class RoomState(db.Model):
+    __tablename__ = 'room_states'
+    userid = db.Column(db.String(33), primary_key=True)
+
+    def __init__(self, userid):
+        self.userid = userid
 
 
 line_bot_api = LineBotApi(os.getenv('LINE_ACCESS_TOKEN', None))
@@ -59,14 +67,19 @@ def callback():
     return 'OK'
 
 
+waiting_room_notification = False
+room_number = -1
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.message.text == '[設定房務頻道]':
+        RoomState.query.filter_by(userid=event.source.user_id).delete()
         line_bot_api.reply_message(event.reply_token, TemplateSendMessage(
             alt_text='請設定房務通知的頻道',
             template=ButtonsTemplate(
                 title='房務頻道',
-                text='請設定您的職位，以便第一時間推播有關客房狀態~',
+                text='請設定您的職位，以便第一時間推播客房狀態~',
                 actions=[
                     MessageTemplateAction(
                         label='櫃台人員',
@@ -83,21 +96,44 @@ def handle_message(event):
                 ]
             )
         ))
-    elif '設定頻道' in event.message.text:
+    elif '[設定頻道' in event.message.text:
+        RoomState.query.filter_by(userid=event.source.user_id).delete()
         code = int(event.message.text[5])
         try:
             user = Personnel.query.filter_by(userid=event.source.user_id).first()
             if user is not None:
                 user.job_code = code
             else:
-                db.session.add(Personnel(event.source.user_id, line_bot_api.get_profile(event.source.user_id).display_name, code))
+                db.session.add(
+                    Personnel(event.source.user_id, line_bot_api.get_profile(event.source.user_id).display_name, code))
             db.session.commit()
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='設定完成~'))
         except LineBotApiError:
-            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='無法'))
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='設定頻道失敗!'))
+    elif event.message.text == '[傳送客房通知]':
+        job = Personnel.query.filter_by(userid=event.source.user_id).first().job_code
+        if job == 3:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='此職位暫無通知其他人員之權限'))
+        user = RoomState.query.filter_by(userid=event.source.user_id).first()
+        if user is None:
+            db.session.add(RoomState(event.source.user_id))
+            db.session.commit()
 
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+        if job == 1:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請填入已退房的房間號碼:'))
+        elif job == 2:
+            line_bot_api.reply_message(event.reply_token, TextSendMessage(text='請填入整理完成的房間號碼:'))
+    elif event.message.text.isnumeric():
+        job = Personnel.query.filter_by(userid=event.source.user_id).first().job_code
+        user = RoomState.query.filter_by(userid=event.source.user_id).first()
+        if user is not None:
+            RoomState.query.filter_by(userid=event.source.user_id).delete()
+            for personnel in Personnel.query.all():
+                if personnel.job_code == job + 1:
+                    line_bot_api.push_message(to=personnel.userid, messages=TextSendMessage(text='[通知] {}號房已{}'.format(event.message.text, '退房' if job == 1 else '整理完畢')))
+            db.session.commit()
+    else:
+        RoomState.query.filter_by(userid=event.source.user_id).delete()
 
 
 if __name__ == "__main__":
